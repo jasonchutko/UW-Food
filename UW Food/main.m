c7ec8eaa47954f1d918e40ae34af0fa20142ccc8
Jason Chutko <jason@Jasons-MacBook-Pro.local>
2012-07-20 23:21:38 -0400

    Initial Commit

---
+++ UW Food/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UW Food
+//
+//  Created by Jason Chutko on 2012-07-20.
+//  Copyright (c) 2012 Jason Chutko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
